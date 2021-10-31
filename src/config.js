89b88ce92c86a4328810d1480794d8aa34c57967
mightybearil <93486932+mightybearil@users.noreply.github.com>
2021-10-31 19:32:05 +0200

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,127 @@
+"use strict";
+
+const path = require("path");
+const isLocal = typeof process.pkg === "undefined";
+const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
+const { MODE } = require(path.join(basePath, "constants/blend_mode.js"));
+const { NETWORK } = require(path.join(basePath, "constants/network.js"));
+
+const network = NETWORK.eth;
+
+// General metadata for Ethereum
+const namePrefix = "MB Collection";
+const description = "Mightybear Test Collection";
+const baseUri = "ipfs://QmU7uPk53zw5P9UyfH6PR3pvmxQHQDtRjeynaTWFGNnL8q";
+
+const solanaMetadata = {
+  symbol: "YC",
+  seller_fee_basis_points: 2000, // Define how much % you want from secondary market sales 1000 = 10%
+  external_url: "",
+  creators: [
+    {
+      address: "",
+      share: 100,
+    },
+  ],
+};
+
+// If you have selected Solana then the collection starts from 0 automatically
+const layerConfigurations = [
+  {
+    growEditionSizeTo: 10,
+    layersOrder: [
+      { name: "Background" },
+      { name: "Body" },
+      { name: "Eyes" },
+      { name: "Mouth" },
+      { name: "Nose" },
+      { name: "Clothing" },
+      { name: "Accessories" },
+      { name: "HeadTop" },
+      { name: "Trait" },
+    ],
+  },
+];
+
+const shuffleLayerConfigurations = false;
+
+const debugLogs = false;
+
+const format = {
+  width: 2000,
+  height: 2000,
+};
+
+const gif = {
+  export: false,
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+};
+
+const text = {
+  only: false,
+  color: "#ffffff",
+  size: 20,
+  xGap: 40,
+  yGap: 40,
+  align: "left",
+  baseline: "top",
+  weight: "regular",
+  family: "Courier",
+  spacer: " => ",
+};
+
+const pixelFormat = {
+  ratio: 2 / 128,
+};
+
+const background = {
+  generate: true,
+  brightness: "80%",
+  static: false,
+  default: "#000000",
+};
+
+const extraMetadata = {};
+
+const rarityDelimiter = "#";
+
+const uniqueDnaTorrance = 10000;
+
+const preview = {
+  thumbPerRow: 5,
+  thumbWidth: 50,
+  imageRatio: format.height / format.width,
+  imageName: "preview.png",
+};
+
+const preview_gif = {
+  numberOfImages: 5,
+  order: "ASC", // ASC, DESC, MIXED
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+  imageName: "preview.gif",
+};
+
+module.exports = {
+  format,
+  baseUri,
+  description,
+  background,
+  uniqueDnaTorrance,
+  layerConfigurations,
+  rarityDelimiter,
+  preview,
+  shuffleLayerConfigurations,
+  debugLogs,
+  extraMetadata,
+  pixelFormat,
+  text,
+  namePrefix,
+  network,
+  solanaMetadata,
+  gif,
+  preview_gif,
+};
